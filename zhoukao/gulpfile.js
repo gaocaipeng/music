86d93c2c1e72e96002b951d21742c332b4860669
gaocaipeng <2540225402@qq.com>
2019-01-02 09:31:25 +0800

    数据开发完成

--- zhoukao/gulpfile.js
+++ zhoukao/gulpfile.js
@@ -10,14 +10,60 @@ var uglify = require('gulp-uglify');
 
 var server = require('gulp-webserver');
 
-var htmlnin = require('gulp-htmlmin');
+var htmlmin = require('gulp-htmlmin');
+
+// var babel = require('gulp-babel');
+
 
 gulp.task('devScss', function() {
     return gulp.src('./src/scss/*.scss')
         .pipe(sass())
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions']
-        }))
+        .pipe(autoprefixer({ browsers: ['last 2 versions'] }))
         .pipe(minCss())
         .pipe(gulp.dest('./src/css'))
 })
+
+gulp.task('watch', function() {
+    return gulp.watch('./src/scss/*.scss', gulp.series('devScss'))
+})
+
+//起服务
+gulp.task('server', function() {
+    return gulp.src('src')
+        .pipe(server({
+            port: 9090
+        }))
+})
+
+//开发环境
+gulp.task('dev', gulp.series('devScss', 'server', 'watch'))
+
+//build 
+// gulp.task('bUglify', function() {
+//     return gulp.src('./src/js/*.js')
+//         .pipe(babel({
+//             presets: ['@babel/env']
+//         }))
+//         .pipe(uglify())
+//         .pipe(gulp.dest('./build/js'))
+// })
+
+gulp.task('bCopy', function() {
+    return gulp.src('./src/js/libs/*.js')
+        .pipe(gulp.dest('./build/js/libs'))
+})
+
+//线上环境css
+gulp.task('bCss', function() {
+    return gulp.src('./src/css/*.css')
+        .pipe(gulp.dest('./build/css'))
+})
+
+gulp.task('bHtml', function() {
+    return gulp.src('./src/**/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('./build'))
+})
+
+//线上环境
+gulp.task('build', gulp.parallel('bCopy', 'bCss', 'bHtml'))
---
+++ zhoukao/src/js/page/index.js
@@ -0,0 +1,5 @@
+require(['./js/config.js'], function() {
+    require(['jquery', 'flex'], function($, flex) {
+        alert('连接舒畅')
+    })
+})
